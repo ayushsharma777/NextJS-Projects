39c50e9b76cabb87a9c5f369ddce76c80fe332e7
ayush <ayuyshsharma1998@gmail.com>
2024-02-12 23:32:17 +0530

    remove carousel

--- thebabycart/app/components/FeaturedProducts.jsx
+++ thebabycart/app/components/FeaturedProducts.jsx
@@ -4,57 +4,57 @@ import FeaturedTitle from "./FeaturedTitle";
 import ProductItem from "./ProductItem";
 import ShowAllText from "./ShowAll-Text";
 
-import "../../node_modules/owl.carousel/dist/assets/owl.carousel.css";
-import "../../node_modules/owl.carousel/dist/assets/owl.theme.default.css";
-import dynamic from "next/dynamic";
+// import "../../node_modules/owl.carousel/dist/assets/owl.carousel.css";
+// import "../../node_modules/owl.carousel/dist/assets/owl.theme.default.css";
+// import dynamic from "next/dynamic";
 
-const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
-  ssr: false,
-});
+// const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+//   ssr: false,
+// });
 
-const Responsive = {
-  0: {
-    items: 1,
-    margin: 5,
-  },
-  425: {
-    items: 1,
-    margin: 5,
-  },
-  768: {
-    items: 2,
-    margin: 5,
-  },
-  1024: {
-    items: 3,
-    margin: 5,
-  },
-  1440: {
-    items: 4,
-    margin: 5,
-  },
-  1600: {
-    items: 5,
-    margin: 5,
-  },
-  2560: {
-    items: 7,
-    margin: 5,
-  },
-};
+// const Responsive = {
+//   0: {
+//     items: 1,
+//     margin: 5,
+//   },
+//   425: {
+//     items: 1,
+//     margin: 5,
+//   },
+//   768: {
+//     items: 2,
+//     margin: 5,
+//   },
+//   1024: {
+//     items: 3,
+//     margin: 5,
+//   },
+//   1440: {
+//     items: 4,
+//     margin: 5,
+//   },
+//   1600: {
+//     items: 5,
+//     margin: 5,
+//   },
+//   2560: {
+//     items: 7,
+//     margin: 5,
+//   },
+// };
 
-var $ = require("jquery");
+// var $ = require("jquery");
 
-if (typeof window !== "undefined") {
-  window.$ = window.jQuery = require("jquery");
-}
+// if (typeof window !== "undefined") {
+//   window.$ = window.jQuery = require("jquery");
+// }
 
 const FeaturedProducts = ({ title, tag, items }) => {
   return (
     <section>
       <FeaturedTitle textHead={title} textTag={tag}></FeaturedTitle>
       <div className="featured-Item-Container">
-        <OwlCarousel responsive={Responsive}>
+        {/* <OwlCarousel responsive={Responsive}> */}
           {items.map((products, index) => (
             <ProductItem
               key={index}
@@ -67,7 +67,7 @@ const FeaturedProducts = ({ title, tag, items }) => {
               itemRating={products.itemRating}
             />
           ))}
-        </OwlCarousel>
+        {/* </OwlCarousel> */}
       </div>
 
       <ShowAllText></ShowAllText>

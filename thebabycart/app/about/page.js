e549583d70933d055d5ce27771c9ec427207fba3
ayush <ayuyshsharma1998@gmail.com>
2024-02-17 00:23:58 +0530

    about me ui fix

--- thebabycart/app/about/page.js
+++ thebabycart/app/about/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 function page() {
   return (
     <div className="d-flex align-items-center justify-content-center flex-column">
-      <div>Developed by Ayush Sharma</div>
+      <div className="mt-5">Developed by Ayush Sharma</div>
       <a href="https://www.linkedin.com/in/ayushsharma77/" target="_blank">
-        <div className=" mt-5 aboutme-img">
+        <div className=" mt-5 aboutme-img mb-5">
           <Image height={100} width={100} src={"/images/linkedin.png"}></Image>
         </div>
       </a>
